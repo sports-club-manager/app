3c29141da49b7bafe49e5b16f94cc3f96595d912
Darren Davison <darren@davisononline.org>
2022-06-20 01:53:01 +0100

    stage 2 updates and leaguetable POSTing

--- src/routes/results/[id].js
+++ src/routes/results/[id].js
@@ -1,4 +1,4 @@
-import { updateResult, findResult, removeResult } from "$lib/db";
+import { updateResult, findResult, removeResult, updateStageTwo } from "$lib/db";
 import { io } from "$lib/socket-client";
 
 export const get = async ({ params }) => {
@@ -9,15 +9,29 @@ export const get = async ({ params }) => {
 
 export const put = async ({ params, request }) => {
     console.log(`request.PUT ${params.id}`);
-    const updated = await updateResult(params.id, await request.json());
+    const result = await updateResult(params.id, await request.json());
 
     // broadcast the updated result on the socket
-    console.log(`broadcasting updated result: ${updated}`);
-    io.emit("save-result", updated);
+    console.log(`broadcasting updated result: ${result}`);
+    io.emit("save-result", result);
+
+    // search and replace stage2 tag
+    if ("stage2Tag" in result && result.stage2Tag !== undefined) {
+        console.debug(`Searching for stage 2 target ${result.stage2Tag}`);
+        var winner = result.homeTeam;
+        if (result.awayPens > result.homePens || result.awayGoals > result.homeGoals) {
+            winner = result.awayTeam;
+        }
+        const updated = await updateStageTwo(result.stage2Tag, winner);
+        for (var i = 0; i < updated.length; i++) {
+            console.debug(`Emiting updated stage2 result ${JSON.stringify(updated[i])}`);
+            io.emit("save-result", updated[i]);
+        }
+    }
 
     return {
         status: 200,
-        body: updated,
+        body: result,
     };
 };
 
