de20fac9028f6ebabad335cd014c9ded041060fd
Darren Davison <darren@davisononline.org>
2023-04-28 01:44:17 +0100

    added compression to the http responses in prod

--- src/lib/server/node-server.js
+++ src/lib/server/node-server.js
@@ -5,24 +5,25 @@
 import { handler } from "./handler.js";
 import { createServer } from "http";
 import express from "express";
+import compression from "compression";
 import injectSocketServer from "./socket-server.js";
+import { logger } from "./logger.js";
 
 const app = express();
 const httpServer = createServer(app);
 injectSocketServer(httpServer);
 
 app.disable("x-powered-by");
+app.use(compression());
 
 // let SvelteKit handle everything, including serving prerendered pages and static assets
 app.use(handler);
 
 // fallback error handler
 app.use((err, req, res) => {
-    var e = app.get("env") === "development" ? err : {};
     logger.error(err);
     res.status(err.status || 500).json({
         message: err.message,
-        error: e,
     });
 });
 
